5e5c4b4b3d586fb0256644eae0bd1a32fb162a1c
blackrabbit <rabbitblack944@gmail.com>
2022-07-20 15:14:52 +0800

    移除一个不用的方法

--- helper/common.js
+++ helper/common.js
@@ -239,12 +239,4 @@ export const isUnloginAllowedPage = function (route) {
             || route == '/user/forget')
 }
 
-import {getConfig} from 'helper/config'
 
-export const getItemImage = function (item,width = 500) {
-    if (!item.get('local_path')) {
-        return item.get('http_image_url');
-    }
-    let api = getConfig('API');
-    return api + '/v1/item/image?contract_address=' + item.get('contract_address') + '&token_id=' + item.get('token_id') + '&width=' + width;
-}
